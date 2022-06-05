35e4ac181b42d4db60c46348a1afd38b4f8d5af6
TobyPrabhat <devprodi.start@gmail.com>
2022-06-05 18:03:45 +0530

    Adding navbar component

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+
+const about = () => {
+  return (
+    <div>
+      <h1>This is About Page!</h1>
+    </div>
+  )
+}
+
+export default about
---
+++ pages/components/navbar.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Logo from "./Logo.png"
+import Image from 'next/image'
+import styles from "./navbar.module.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faIndianRupeeSign,
+  faShoppingCart,
+  faArchive,
+  faBagShopping
+} from "@fortawesome/free-solid-svg-icons";
+export default function navbar(){
+  return(
+    <div className={styles.NavbarContent}> 
+      <nav className={styles.Navbar}>
+        <Image src={Logo} alt="alt" width="85" height="85"/>
+      <div className={styles.SocialInfo}>
+      <a href="#"><FontAwesomeIcon icon={faIndianRupeeSign} className = {styles.icon1}/></a>
+      <a href="#">
+      <FontAwesomeIcon icon = {faShoppingCart} className = {styles.icon2}/>
+      </a>
+      <a href="#">
+      <FontAwesomeIcon icon={faArchive} className = {styles.icon3} />
+      </a>
+      <a href="#">
+      <FontAwesomeIcon icon = {faBagShopping} className = {styles.icon4} />
+      </a>
+      </div>
+      <a href="./Home"><h2>Home</h2><span></span></a>
+      <a href="./about"><h2>About</h2><span></span></a>
+      </nav>
+    </div>
+  )
+}

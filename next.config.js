eb38fb11b72d85f166cfef68c78db52b2ceb3353
TobyPrabhat <devprodi.start@gmail.com>
2022-06-10 12:50:49 +0530

    for image optimisation

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  target: "serverless",
+  images: {
+        loader: "cloudinary",
+        path: ""
+    },
 }
 
 module.exports = nextConfig
